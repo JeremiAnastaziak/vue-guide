296ce1f0bae2e1de1cb638aeb4c35d8b12ad5621
Jeremi Anastaziak <jeremi.anastaziak@allegro.pl>
2018-04-22 14:05:40 +0200

    Working with render method

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <template>
   <div id="app">
-    <Playground />
-    <Post
-      v-for="post in posts"
-      v-bind:text="post.text"
-    />
+    <Wrapper tag-name="section">
+      <Playground />
+      <Post v-for="post in posts" v-bind:text="post.text" v-bind:key="post.text" />
+    </Wrapper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Playground from './components/Playground.vue'
-import Post from './components/Post.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import Playground from './components/Playground.vue'
+  import Post from './components/Post.vue'
+  import Wrapper from './components/Wrapper.vue';
 
-export default {
-  name: 'app',
-  data: () => ({
-    posts: [
-      {
-        text: 'New article'
-      },
-      {
-        text: 'Another article'
-      }
-    ]
-  }),
-  components: {
-    HelloWorld,
-    Playground,
-    Post
+  export default {
+    name: 'app',
+    data: () => ({
+      posts: [{
+          text: 'New article'
+        },
+        {
+          text: 'Another article'
+        }
+      ]
+    }),
+    components: {
+      HelloWorld,
+      Playground,
+      Post,
+      Wrapper
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    name: 'Wrapper',
+    render: function(createElement) {
+      return createElement(
+        this.tagName, // tag name
+        {
+          class: 'wrapper',
+          style: {
+            color: '#222',
+          },
+        },
+        this.$slots.default // array of children
+      )
+    },
+    props: {
+      tagName: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style>
+  .wrapper {
+    border: 1px dotted darksalmon;
+    border-radius: 5px;
+  }
+</style>
+
